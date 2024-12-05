15c985a192876e99aa56b1d58672bb0582246766
afsercreators <afsercreators@gmail.com>
2024-12-05 17:00:23 +0600

    5Dec

--- src/Components/StudentsList.jsx
+++ src/Components/StudentsList.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import axios from "axios";
 import { MdDeleteForever } from "react-icons/md";
 import { FaEdit } from "react-icons/fa";
 import { MdOutlinePreview } from "react-icons/md";
 
 function StudentsList() {
   const navigate = useNavigate();
+  const [data, setData] = useState([]);
 
   const StudentView = () => {
     navigate("/view");
   };
+
+  useEffect(() => {
+    const getStudent = async () => {
+      const response = await axios.get(`http://localhost:8000/api/student`);
+
+      setData(response.data);
+    };
+
+    getStudent();
+  }, []);
 
   return (
     <div className="my-5 container shadow">
@@ -18,27 +29,29 @@ function StudentsList() {
         <thead>
           <tr>
             <th scope="col">#</th>
-            <th scope="col">Student Roll</th>
             <th scope="col">Student Name</th>
-            <th scope="col">Father Name</th>
+            <th scope="col">Student Father Name</th>
+            <th scope="col">Date of Birth</th>
             <th scope="col">Action</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>01</td>
-            <td>Mr. Rohim</td>
-            <td>Mr. Korim</td>
-            <td>
-              <MdOutlinePreview
-                onClick={(e) => StudentView()}
-                className="fs-5 mx-2 cursor"
-              />
-              <FaEdit className="fs-5 mx-2 cursor" />
-              <MdDeleteForever className="fs-5 mx-2 cursor" />
-            </td>
-          </tr>
+          {data.map((item, index) => (
+            <tr key={item._id}>
+              <td scope="row">{index + 1}</td>
+              <td scope="row">{item.name}</td>
+              <td scope="row">{item.fatherName}</td>
+              <td scope="row">{item.dateofBirth}</td>
+              <td>
+                <MdOutlinePreview
+                  onClick={(e) => StudentView()}
+                  className="fs-5 mx-2 cursor"
+                />
+                <FaEdit className="fs-5 mx-2 cursor" />
+                <MdDeleteForever className="fs-5 mx-2 cursor" />
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
